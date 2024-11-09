1d678bd7bebbb8d41bbc633f2cafc9f194cf34b2
sarf01k <isaacksarfo1@gmail.com>
2024-11-09 15:14:09 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace BirthdayCountdown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Today;
+            DateTime date;
+
+            Console.Write("Enter your birth date (DD/MM/YYYY): ");
+
+            DateTime.TryParse(Console.ReadLine(), out date);
+
+            DateTime nextBDay = Convert.ToDateTime(date.AddYears(today.Year - date.Year + 1));
+            int daysLeft = nextBDay.Subtract(today).Days;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n{0} days till your birthday!!! ({1})", daysLeft, nextBDay.ToLongDateString());
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# Birthday Countdown Console App
+
+A simple console application built in C# to display the remaining days until the user's next birthday.
+
+## Features
+- Asks for the user’s birth date.
+- Calculates the time left until the next birthday.
+- Displays the countdown in days.
+
+## Getting Started
+
+### Prerequisites
+- [.NET SDK](https://dotnet.microsoft.com/download) installed on your machine.
+
+### Installation
+1. Clone this repository:
+   ```bash
+   git clone https://github.com/yourusername/birthday-countdown-app.git
