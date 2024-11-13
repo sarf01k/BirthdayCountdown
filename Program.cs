f05b84e60a8a6235c5f1ead46ba0f3035bcdfff9
sarf01k <isaacksarfo1@gmail.com>
2024-11-13 13:56:51 +0000

    refactored

--- Program.cs
+++ Program.cs
@@ -1,23 +1,28 @@
-﻿namespace BirthdayCountdown
+﻿DateTime today = DateTime.Today;
+DateTime date;
+
+Console.Write("Enter your birth date: ");
+
+if (DateTime.TryParse(Console.ReadLine(), out date))
 {
-    class Program
+    if (date > today)
     {
-        static void Main(string[] args)
-        {
-            DateTime today = DateTime.Today;
-            DateTime date;
-
-            Console.Write("Enter your birth date (DD/MM/YYYY): ");
-
-            DateTime.TryParse(Console.ReadLine(), out date);
-
-            DateTime nextBDay = Convert.ToDateTime(date.AddYears(today.Year - date.Year + 1));
-            int daysLeft = nextBDay.Subtract(today).Days;
-
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("\n{0} days till your birthday!!! ({1})", daysLeft, nextBDay.ToLongDateString());
-            Console.ForegroundColor = ConsoleColor.Gray;
-            Console.ReadLine();
-        }
-    }
+        Console.WriteLine("The date entered is in the future. Please enter a valid date.");
+        return;
+    }    
 }
+else
+{
+    Console.WriteLine("Enter a valid date.");
+    return;
+}
+
+
+
+DateTime nextBDay = Convert.ToDateTime(date.AddYears(today.Year - date.Year + 1));
+int daysLeft = nextBDay.Subtract(today).Days;
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"\n{daysLeft} days till your birthday!!! ({nextBDay.ToLongDateString()})");
+Console.ForegroundColor = ConsoleColor.Gray;
+Console.ReadLine();
